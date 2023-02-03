91d185124da431aae1a6968c57d095ce2a17dace
Riatte <95982898+Riatte@users.noreply.github.com>
2023-02-03 16:40:45 +0000

    Deploying to gh-pages from @ Riatte/goit-js-hw-09@73d29ba91b8ba71e23c4ba93d10e94d4c7795777 🚀

---
+++ 01-color-switcher.11b1fbc3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null;function d(t){e.disabled=t,n.disabled=!t}n.disabled=!0,e.addEventListener("click",(function(){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),d(!0)})),n.addEventListener("click",(function(){clearInterval(o),d(!1)}));
+//# sourceMappingURL=01-color-switcher.11b1fbc3.js.map
